712e583493235cf7c8ba17f1d76b910532585215
Sam Tay <sam.chong.tay@gmail.com>
2016-12-05 16:17:09 -0500

    Stub out some more of the actual dm execution

--- src/Dockmaster.hs
+++ src/Dockmaster.hs
@@ -19,24 +19,32 @@ import Prelude hiding (FilePath)
 import qualified Data.Text as T
 default (T.Text)
 
--- | Possibly provide sum type data = DcUp | DcDown | ... with to string method
 type DCCommand = T.Text
 
 -- | Runs docker-compose commands against resolved composition locations
 -- See usage docs for more info. Tries to find a dockmaster.yml file based on
 -- the initial path argument
-dm :: FilePath -> DCCommand -> [T.Text] -> Sh (T.Text)
+-- TODO monad >>= and >> the shit out of this to remove the casing structure
+-- REMEMBER you can do `when weAreDone exitSuccess` as control flow in do statement
+dm :: FilePath -> DCCommand -> [T.Text] -> Sh ()
 dm path command args = do
-  gwd <- getWorkDir path
-  case gwd of
-    Left err -> do
-                echo_n_err err
-                return err -- maybe dm should return Either and executable handles printing?
+  eWd <- getWorkDir path
+  case eWd of
+    Left err -> echo_n_err err
     Right wd -> sub $ do
       cd wd
-      dockermachine -- TODO handle Left errors
-      hookWrap command $ dockercompose $ command : args
+      dmYml <- dockmasterYml
+      case dmYml of
+        Left err    -> echo_n_err err
+        Right dmYml -> do
+          let targets = map targetName $ dmTargets dmYml -- primitively just grabbing machine name
+          results <- (flip mapM) targets $ \m -> dockermachine m $ do
+            response <- hookWrap command $ dockercompose $ command : args
+            echo_n response
+          mapM_ echo_n_err $ lefts results
 
+
+-- | Run docker-compose command
 dockercompose :: [T.Text] -> Sh T.Text
 dockercompose = run "docker-compose"
 
@@ -45,8 +53,13 @@ dockercompose = run "docker-compose"
 hookWrap :: DCCommand -> Sh a -> Sh a
 hookWrap = undefined
 
--- | Reads from dockmaster.yml to find machine, attempts to connect
--- dm should HALT when machine is unreachable
-dockermachine :: Sh (Either T.Text ())
-dockermachine = undefined
+-- | Takes machine name and Sh action, and wraps Sh action in scope of
+-- docker-machine env
+dockermachine :: T.Text -> Sh a -> Sh (Either T.Text a)
+dockermachine m action = sub $ do
+  run "docker-machine" ["env", m]
+  -- TODO check if successful before running action
+  if True
+     then return $ Left $ "Could not reach docker machine " <> m
+     else action >>= return . Right
 
