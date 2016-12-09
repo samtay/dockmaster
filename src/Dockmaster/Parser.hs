16a124c757ed4991e838dc7a62eac98607f2b7a6
Sam Tay <sam.chong.tay@gmail.com>
2016-12-09 15:34:59 -0500

    Execute hooks specified in dockmaster.yml
    Also modified `Sh a` and `Sh T.Text` types to `Sh ()`, which makes
    more sense considering we don't know what arbitrary hooks will return,
    and because we allow the option of not passing through to docker-compose
    at all.
    
    Instead, this is now handled by specifically specifying "print_stdout
    True" in the dockercompose subshell execution.

--- src/Dockmaster/Parser.hs
+++ src/Dockmaster/Parser.hs
@@ -13,14 +13,15 @@ module Dockmaster.Parser
 -- Local modules
 import Dockmaster.Utils (eitherWrap)
 import Dockmaster.Config.Parser
+import Dockmaster.Types
 
 -- External modules
 import Data.Yaml
-import Dockmaster.Types
 import qualified Data.ByteString as BS
-
+import Data.HashMap.Lazy (HashMap, lookup, member)
+import Data.Monoid ((<>), mconcat, First(..))
 import Shelly
-import Prelude hiding (FilePath)
+import Prelude hiding (FilePath, lookup)
 import qualified Data.Text as T
 default (T.Text)
 
@@ -35,13 +36,36 @@ dockmasterYml = do
 
 -- | Executes specific dc command pre/post hooks around action argument
 -- (action arg is typically docker-compose command)
-hookWrap :: T.Text -> Sh T.Text -> Sh T.Text
+hookWrap :: T.Text -> Sh () -> Sh ()
 hookWrap dcCmd action = do
   dmYml <- dockmasterYml
-  either return (\cfg -> hookWrap' cfg dcCmd action) dmYml
+  either
+    errorExit                            -- Exit on dockmaster.yml parsing failure
+    (\cfg -> hookWrap' cfg dcCmd action) -- Otherwise execute hooks & docker-compose
+    dmYml
 
 -- | Same thing as 'hookWrap' but accepts 'Dockmaster' config to execute against
-hookWrap' :: Dockmaster -> T.Text -> Sh a -> Sh a
+hookWrap' :: Dockmaster -> T.Text -> Sh () -> Sh ()
 hookWrap' cfg dcCmd action = do
-  let cmdCfg = dmCommands cfg
-  return undefined
+  -- Gather command configuration hooks/pass-through
+  let cmdCfg = lookup dcCmd $ dmCommands cfg
+      pre    = maybe [] ccPreHooks cmdCfg
+      post   = maybe [] ccPreHooks cmdCfg
+      pass   = maybe True ccCompose cmdCfg
+  mapM_ execHook pre  -- Exec pre hooks
+  when pass action    -- Exec docker-compose (unless pass-through = false)
+  mapM_ execHook post -- Exec post hooks
+
+-- | Execute hook
+execHook :: Hook -> Sh ()
+-- Execute "file" hook type
+execHook (File txt)  = do
+  -- Need to force relative path (Shelly oddism)
+  let file = fromText $ if not (T.null txt) && (T.head txt /= '/')
+      then T.append "./" txt
+      else txt
+  bash_ file []
+-- Execute "shell" hook type
+execHook (Shell txt) = unless (T.null txt) $ do
+  let (shCmd:shArgs) = T.words txt
+  run_ (fromText shCmd) shArgs
