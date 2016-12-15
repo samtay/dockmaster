b66a2f6a829524f2b267ec898d61531440e47b8d
Sam Tay <sam.chong.tay@gmail.com>
2016-12-15 13:36:49 -0500

    Clean up: comments/formatting/etc

--- app/Dmc.hs
+++ app/Dmc.hs
@@ -48,20 +48,6 @@ main = execParser opts >>= shelly . runtime
 runtime :: Dmc -> Sh ()
 runtime = undefined
 
-anyfield :: ReadM T.Text
-anyfield = eitherReader inConfig
-
-arrfield :: ReadM T.Text
-arrfield = eitherReader inConfigArr
-
-inConfig :: String -> Either String T.Text
-inConfig "PATHS" = Right "PATHS"
-inConfig arg     = Left $ arg ++ " is not a valid config field."
-
-inConfigArr :: String -> Either String T.Text
-inConfigArr "PATHS" = Right "PATHS"
-inConfigArr arg     = Left $ fromLeft (arg ++ " is not an array type.") (inConfig arg)
-
 -- | Parser for /set/ commands
 setOptions :: ReadM T.Text -> Parser SetOptions
 setOptions optType = SetOptions
@@ -73,15 +59,46 @@ getOptions :: Parser GetOptions
 getOptions = GetOptions
   <$> argument anyfield (metavar "NAME" <> help "Name of the setting to retrieve")
 
+-- | Reader for any config fields
+anyfield :: ReadM T.Text
+anyfield = eitherReader inConfig
+
+-- | Reader for array config fields
+arrfield :: ReadM T.Text
+arrfield = eitherReader inConfigArr
+
+-- | Parse argument for any value type
+inConfig :: String -> Either String T.Text
+inConfig "PATHS" = Right "PATHS"
+inConfig arg     = Left $ arg ++ " is not a valid config field."
+
+-- | Parse argument for array value type
+inConfigArr :: String -> Either String T.Text
+inConfigArr "PATHS" = Right "PATHS"
+inConfigArr arg     = Left $ fromLeft (arg ++ " is not an array type.") (inConfig arg)
+
 -- | Parser for 'Dmc'.
 parser :: Parser Dmc
 parser = subparser
-  (  (command "set" $ commandInfo (Set <$> setOptions anyfield) "Set value")
-  <> (command "get" $ commandInfo (Get <$> getOptions) "Get value")
-  <> (command "unshift" $ commandInfo (Unshift <$> setOptions arrfield) "Unshift value (for arrays)")
-  <> (command "shift" $ commandInfo (Shift <$> getOptions) "Shift value (for arrays)")
-  <> (command "push" $ commandInfo (Push <$> setOptions arrfield) "Push value (for arrays)")
-  <> (command "pop" $ commandInfo (Pop <$> getOptions) "Pop value (for arrays)")
+  (
+     (command "set" $ commandInfo
+       (Set <$> setOptions anyfield)
+       "Set value")
+  <> (command "get" $ commandInfo
+       (Get <$> getOptions)
+       "Get value")
+  <> (command "unshift" $ commandInfo
+       (Unshift <$> setOptions arrfield)
+       "Unshift value (for arrays)")
+  <> (command "shift" $ commandInfo
+       (Shift <$> getOptions)
+       "Shift value (for arrays)")
+  <> (command "push" $ commandInfo
+       (Push <$> setOptions arrfield)
+       "Push value (for arrays)")
+  <> (command "pop" $ commandInfo
+       (Pop <$> getOptions)
+       "Pop value (for arrays)")
   ) 
 
 -- | Generate 'ParserInfo' for 'Dmc'.
