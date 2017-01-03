0d72b08c90bb6ee1a841c2a78506d3772e22ad0e
Sam Tay <sam.chong.tay@gmail.com>
2017-01-03 15:05:30 -0500

    Simplify cli parser

--- app/Dm.hs
+++ app/Dm.hs
@@ -9,75 +9,70 @@ import Options.Applicative
 
 import Prelude hiding (FilePath)
 import Data.Monoid ((<>))
+import Control.Monad (join)
 import Options.Utils (text, filePathOption)
 import qualified Data.Text as T
 
 default (T.Text)
 
--- | Datatype to hold cli options/arguments
-data Dm = Dm
-  { dmCompositionDir :: FilePath
-  , dmVerbose        :: Bool
-  , dmLocal          :: Bool
-  , dmDcCommand      :: T.Text
-  , dmDcOpts         :: [T.Text]
-  } deriving (Eq,Show)
-
 -- | Main runtime
--- Retrieves possible DOCKMASTER_COMPOSITION env variable for use as
+--
+-- Retrieves possible @DOCKMASTER_COMPOSITION@ env variable for use as
 -- default composition, then defers to cli parser & shelly runtime
 main :: IO ()
 main = do
   envCompDir <- shelly $ get_env "DOCKMASTER_COMPOSITION"
   let defaultCompDir = maybe "." T.unpack envCompDir
-   in execParser (opts defaultCompDir) >>= execShelly
+  join . execParser $
+    info (helper <*> parser defaultCompDir)
+    (  fullDesc
+    <> progDesc "Orchestrate your docker-compose"
+    <> header "dm - yaml loving docker compose orchestration"
+    )
+
+-- | Parse CLI opts/args into arguments for 'execDm'
+parser :: String -> Parser (IO ())
+parser defaultCompDir =
+  execDm
+    <$> filePathOption
+        ( long "composition"
+        <> short 'c'
+        <> metavar "PATH"
+        <> showDefault
+        <> value defaultCompDir
+        <> help "Composition directory. Note this can be relative to any directories specified in global config." )
+    <*> switch
+        ( long "verbose"
+        <> short 'v'
+        <> help "Verbose output.")
+    <*> switch
+        ( long "local"
+        <> short 'l'
+        <> help "Execute without connecting to configured docker machine." )
+    <*> argument text
+        ( metavar "COMMAND"
+        <> help "Command to forward to docker-compose.")
+    <*> many (argument text
+        ( metavar "ARGS"
+        <> help "Any arguments/options to forward to docker-compose COMMAND."))
 
 -- | Shelly execution
 --
--- Accepts 'Dm' instance and forwards to 'dm' function
-execShelly :: Dm -> IO ()
-execShelly (Dm c v l command optargs) = shelly
+-- Sets up shelly environment and forwards to 'dockmaster' function
+execDm :: FilePath -- ^ Composition
+       -> Bool     -- ^ Verbosity flag
+       -> Bool     -- ^ Local-only flag
+       -> T.Text   -- ^ Command
+       -> [T.Text] -- ^ Opts/args for Command
+       -> IO ()
+execDm c v l command optargs = shelly
   $ escaping False
   $ subVerbosity v
   $ dockmaster c l command optargs
 
 -- | Accepts a verbosity setting for the subshell
+--
 -- Propogates verbosity to printing options for commands, stdout, stderr
 subVerbosity :: Bool -> Sh a -> Sh a
 subVerbosity v =
   print_stdout v . print_stderr v . print_commands v
-
--- | Parser for 'Dm' opts/args.
--- Takes a string argument as the default composition value
-parser :: String -> Parser Dm
-parser defaultCompDir = Dm
-  <$> filePathOption
-      ( long "composition"
-      <> short 'c'
-      <> metavar "PATH"
-      <> showDefault
-      <> value defaultCompDir
-      <> help "Composition directory. Note this can be relative to any directories specified in global config." )
-  <*> switch
-      ( long "verbose"
-      <> short 'v'
-      <> help "Verbose output.")
-  <*> switch
-      ( long "local"
-      <> short 'l'
-      <> help "Execute without connecting to configured docker machine." )
-  <*> argument text
-      ( metavar "COMMAND"
-      <> help "Command to forward to docker-compose.")
-  <*> many (argument text
-      ( metavar "ARGS"
-      <> help "Any arguments/options to forward to docker-compose COMMAND."))
-
--- | Generate 'ParserInfo' 'Dm'.
--- Takes a string argument as the default composition value
-opts :: String -> ParserInfo Dm
-opts defaultCompDir = info (helper <*> parser defaultCompDir)
-  (  fullDesc
-  <> progDesc "Orchestrate your docker-compose"
-  <> header "dm - yaml loving docker compose orchestration"
-  )
